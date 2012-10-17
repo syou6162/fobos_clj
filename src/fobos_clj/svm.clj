6788ccb350c2a41182a41628a95e5f17ffb1d95e
Yasuhisa Yoshida <syou6162@gmail.com>
2012-10-17 21:42:32 +0900

    SVM protocolをmixinの形で書き変え

--- src/fobos_clj/svm.clj
+++ src/fobos_clj/svm.clj
@@ -1,5 +1,6 @@
 (ns fobos_clj.svm
-  (:use [fobos_clj.fobos]))
+  (:use [fobos_clj.fobos]
+        [fobos_clj.util]))
 
 (defn margin [weight fv y]
   (* (dotproduct weight fv) y))
@@ -10,18 +11,23 @@
 			  (* y xi scale))))
 	  weight fv))
 
-(defrecord SVM
-  [examples weight eta lambda]
+(defrecord SVM [examples weight eta lambda t last-update])
+
+(extend SVM
   Fobos
-  (update-weight [_ iter]
-		 (let [example-size (count examples)]
-		   (assoc _ :weight
-			  (l1-regularize
-			   (reduce (fn [w [y fv]]
-				     (if (< (margin w fv y) 1.0)
-				       (muladd w fv y eta)
-				       w))
-				   weight examples)
-			   iter example-size lambda))))
-  (classify [_ fv]
-	    (if (> (dotproduct weight fv) 0.0) 1 -1)))
+  (merge default-io-fns
+         {:regularize-weigth l1-regularize
+          :update-weight (fn [_]
+                           (reduce
+                            (fn [new-svm [^long y fv]]
+                              (let [new-w (if (< (margin (:weight new-svm) fv y) 1.0)
+                                            (muladd (:weight new-svm) fv y (:eta new-svm))
+                                            (:weight new-svm))]
+                                (regularize-weigth (assoc new-svm :weight new-w) fv)))
+                            _ (:examples _)))
+          :classify (fn ^long [_ fv] ;; return 1 or -1
+                      (if (> (dotproduct (:weight _) fv) 0.0) 1 -1))}))
+
+(defn make-SVM
+  ([] (make-model "fobos_clj.svm.SVM"))
+  ([examples eta lambda] (make-model "fobos_clj.svm.SVM" examples eta lambda)))
