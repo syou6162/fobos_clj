57854c23a51c536c8e15a9d5bd0573e0f6bc2eca
Yasuhisa Yoshida <syou6162@gmail.com>
2012-10-17 21:34:52 +0900

    Fobosのprotocolを変更

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject fobos_clj "0.0.2"
   :description "Implementation of Forward Backward Splitting"
   :dependencies [[org.clojure/clojure "1.3.0"]
+		 [org.clojure/clojure-contrib "1.2.0"]
+                 [info.yasuhisay/clj-utils "0.1.0-SNAPSHOT"]
                  [org.clojure/core.memoize "0.5.1"]]
   :dev-dependencies [[swank-clojure "1.3.2"]]
   :jvm-opts ["-Xmx20g" "-server" "-Dfile.encoding=UTF-8"]
--- src/fobos_clj/fobos.clj
+++ src/fobos_clj/fobos.clj
@@ -1,8 +1,18 @@
+(ns fobos_clj.fobos
+  (:use [clj-utils.io :only (serialize deserialize)]))
+
 (use '[clojure.core.memoize])
 
 (defprotocol Fobos
-  (update-weight [_ iter])
-  (classify [_ fv]))
+  (regularize-weigth [_ fv])
+  (update-weight [_])
+  (classify [_ fv])
+  (save-model [_ filename])
+  (load-model [_ filename]))
+
+(def default-io-fns
+  {:save-model (fn [_ filename] (serialize _ filename))
+   :load-model (fn [_ filename] (deserialize filename))})
 
 (defn clip-by-zero ^double [^double a ^double b]
   "(clip-by-zero a b) = sign(a) max(|a| - b, 0)"
@@ -31,3 +41,31 @@
      (double (get-Lambda-t t 0.0))))
 
 (def get-Lambda-t (memo get-Lambda-t))
+
+(defn l1-regularize ^Fobos [{examples :examples weight :weight eta :eta
+                             lambda :lambda t :t last-update :last-update :as _}
+                            fv]
+  "return Fobos. 'weight' and 'last-update' are updated by regularization."
+  (let [fv-count (count fv)
+        Lambda-t (get-Lambda-t t)]
+    (loop [fv-idx 0
+           weight (transient weight)
+           last-update (transient last-update)]
+      (if (= fv-idx fv-count)
+        (-> _
+            (assoc :weight (persistent! weight))
+            (assoc :t (inc t))
+            (assoc :last-update (persistent! last-update)))
+        (let [[k v] (nth fv fv-idx)
+              old-w-k (get weight k 0.0)
+              t0 (get last-update k 0)
+              Lambda-t0 (get-Lambda-t t0)
+              lambda-hat (* (- Lambda-t Lambda-t0) lambda)
+              new-w-k (clip-by-zero old-w-k lambda-hat)]
+          (recur (inc fv-idx)
+                 (if (zero? new-w-k)
+                   (dissoc! weight k)
+                   (assoc! weight k new-w-k))
+                 (if (zero? new-w-k)
+                   last-update
+                   (assoc! last-update k t))))))))
