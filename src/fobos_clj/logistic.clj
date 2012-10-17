86edfc54ad5a180c692597e42b618b882a81e5d3
Yasuhisa Yoshida <syou6162@gmail.com>
2012-10-17 21:40:51 +0900

    Logistic protocolをmixinの形で書き変えた

--- src/fobos_clj/logistic.clj
+++ src/fobos_clj/logistic.clj
@@ -1,5 +1,6 @@
 (ns fobos_clj.logistic
-  (:use [fobos_clj.fobos]))
+  (:use [fobos_clj.fobos]
+        [fobos_clj.util]))
 
 (defn logistic-deriv [y inner-product xi]
   (* (- y)
@@ -16,20 +17,25 @@
 			   (logistic-deriv y inner-product xi)))))
 	    weight fv)))
 
-(defrecord Logistic
-  [examples weight eta lambda]
+(defrecord Logistic [examples weight eta lambda t last-update])
+
+(extend Logistic
   Fobos
-  (update-weight [_ iter]
-		 (let [example-size (count examples)]
-		   (assoc _ :weight
-			  (l1-regularize
-			   (reduce (fn [w [y fv]]
-				     (muldiff w fv y eta))
-				   weight examples)
-			   iter example-size lambda))))
-  (classify [_ fv]
-	    (letfn [(prob [fv]
-			  (let [inner-prod (dotproduct weight fv)]
-			    (/ 1.0
-			       (+ 1.0 (Math/exp (- inner-prod))))))]
-	      (if (> (prob fv) 0.5) 1 -1))))
+  (merge default-io-fns
+         {:regularize-weigth l1-regularize
+          :update-weight (fn [{examples :examples :as _}]
+                           (reduce
+                            (fn [new-logistic [^long y fv]]
+                              (let [new-w (muldiff (:weight new-logistic) fv y (:eta new-logistic))]
+                                (regularize-weigth (assoc new-logistic :weight new-w) fv)))
+                            _ examples))
+          :classify (fn ^long [{weight :weight :as _} fv] ;; return 1 or -1
+                      (letfn [(prob [fv]
+                                (let [inner-prod (dotproduct weight fv)]
+                                  (/ 1.0
+                                     (+ 1.0 (Math/exp (- inner-prod))))))]
+                        (if (> (prob fv) 0.5) 1 -1)))}))
+
+(defn make-Logistic
+  ([] (make-model "fobos_clj.logistic.Logistic"))
+  ([examples eta lambda] (make-model "fobos_clj.logistic.Logistic" examples eta lambda)))
